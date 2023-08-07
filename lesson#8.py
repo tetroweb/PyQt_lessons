70aa88c921c089db88e91ea8945ecefb26b3ce24
Berkant Zor <berkantzor060@gmail.com>
2023-08-08 01:51:03 +0300

    Update lesson#8.py

--- lesson#8.py
+++ lesson#8.py
@@ -27,8 +27,31 @@ class Main(QWidget):
         self.combo.setCurrentIndex(2)
         # self.combo.setCurrentText("Pencilcase")
         
+        self.combo.insertItem(1,"İnput")
+        self.combo.insertSeparator(2)
+        self.combo.clear()
+        
+        self.radio1 = QRadioButton("İstanbul",self)
+        self.radio2 = QRadioButton("Antalya",self)
+        
+        self.radio2.move(80,0)
+        
+        self.radio1.clicked.connect(self.listing)
+        self.radio2.clicked.connect(self.listing)
+        
+        
+        
         self.resize(800,500)
         self.show()
+        
+    def listing(self):
+        if self.radio1.isChecked():
+            self.combo.clear()
+            self.combo.addItems(d_city["İstanbul"])
+        if self.radio2.isChecked():
+            self.combo.addItems(d_city["Antalya"])
+            
+        
         
         
 app = QApplication([])
