2660d1ed5ff36efe5c2d9d1dd0ea58c0b45f0705
Berkant Zor <berkantzor060@gmail.com>
2023-08-09 00:08:47 +0300

    Update lesson#10.py

--- lesson#10.py
+++ lesson#10.py
@@ -3,29 +3,27 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import * 
 
-#QTextEdit'in QLineEdit'ten farkı birden çok satır içermesi
 class Main(QWidget):
     def __init__(self):
         super().__init__()
         self.tex_edit = QTextEdit(self)
-        self.tex_edit.setText("this is the sample write")
+        self.tex_edit.setText("this is the sample text")
         
-        
-        #html kodlarını olduğu gibi gösterir
+        # Displays HTML code as is
         self.plain_text = QPlainTextEdit(self)
-        self.plain_text.setPlainText("this is the sample write")
-        self.plain_text.move(400,0)
+        self.plain_text.setPlainText("this is the sample text")
+        self.plain_text.move(400, 0)
         
-        self.button = QPushButton("Insert",self)
+        self.button = QPushButton("Insert", self)
         self.button.clicked.connect(self.insert)
-        self.button.move(0,400)
-        self.resize(800,500)
+        self.button.move(0, 400)
+        self.resize(800, 500)
         self.show()
         
     def insert(self):
-        # self.tex_edit.insertHtml("<b>bold write</b>")
-        self.tex_edit.setFontFamily("Verdana")#seçilmiş metinler için geçerli
-        self.tex_edit.setFontPointSize(10)#seçilmiş metinler için geçerlidir
+        # self.tex_edit.insertHtml("<b>bold text</b>")
+        self.tex_edit.setFontFamily("Verdana")  # Applies to selected text
+        self.tex_edit.setFontPointSize(10)  # Applies to selected text
 
 
 app = QApplication([])
