bda6f518dbc0b38c3d1139a1b3b03d48b0cfefa1
Berkant Zor <berkantzor060@gmail.com>
2023-08-19 02:44:22 +0300

    Update lesson#7.py

--- lesson#7.py
+++ lesson#7.py
@@ -14,13 +14,13 @@ class Main(QWidget):
         self.gender_label.setFrameShape(QFrame.Box)
         self.gender_label.setGeometry(0,0,100,100)
         
-        self.radio1 = QRadioButton("Male",self.gender_label)
+        self.radio1 = QRadioButton("Male",self.gender_label)#-> if use radio button once check properties use same family for radio button members
         self.radio1.move(20,22)
         self.radio2 = QRadioButton("Female",self.gender_label)
         self.radio2.move(20,40)
         
         self.label = QLabel("Which  country have you been ?",self)
-        self.check1 = QCheckBox("Denizli",self)
+        self.check1 = QCheckBox("Denizli",self)#-> check box provide more than check for the questions
         self.check2 = QCheckBox("Adana",self)
         self.check3 = QCheckBox("İstanbul",self)
         
@@ -29,13 +29,13 @@ class Main(QWidget):
         self.check2.move(90,120)
         self.check3.move(160,120)
         
-        self.check3.setCheckable(False)# seçilebilme özelliği kalkar
+        self.check3.setCheckable(False)# destroy the choose ability
         
-        self.check2.toggled.connect(self.check)#seçenek seçildiğinde True döndürür
+        self.check2.toggled.connect(self.check)#When an option is selected, it returns True.
         
         self.check2.setChecked(True)
         
-        boolean =self.check1.isChecked()#koşullu seçenekler yazmamız için butonun seçlip seçilmediğine bakar
+        boolean =self.check1.isChecked()#It checks whether the button is selected for conditional options
         print(boolean)
         
         self.resize(800,500)
